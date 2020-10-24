a5a600d192502b0dc38f4cafaec2003fa0c713eb
= <=>
2020-10-24 17:12:52 -0300

    Criando último componente da HomeScreen.

---
+++ src/components/Banner/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import {Container, Img, Details, Title, Description} from './styles';
+
+import img13 from '../../images/13.png';
+
+export default function Banner() {
+    return (
+        <Container>
+            <Details>
+                <Title>Cobre um amigo</Title>
+                <Description>Matenha suas parecerias em dia, use o PicPay para fazer suas cobranças.</Description>
+            </Details>
+            <Img source={img13} />
+        </Container>
+    );
+}
